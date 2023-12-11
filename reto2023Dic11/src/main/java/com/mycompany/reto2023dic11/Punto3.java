81438d33e14a283f34e24fda6f6b70c2f3339a6e
Kevin Jimenez Delgado <122753141+Kevin-Jimenez-D@users.noreply.github.com>
2023-12-11 13:56:58 -0500

    feat: :fire: I inserted the point 4th
    In the folder there is the point fourth incomplete

--- reto2023Dic11/src/main/java/com/mycompany/reto2023dic11/Punto3.java
+++ reto2023Dic11/src/main/java/com/mycompany/reto2023dic11/Punto3.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.reto2023dic11;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,40 @@ public class Punto3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Letras minusculas por defecto
+        String minusculas= "qwertyuiopasdfghjklzxcvbnm";
+        boolean minusculaEncontrada= false;
+        
+        //Letras mayusculas
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Deseas que tu contrasena tenga mayusculas? Coloca SI en caso afirmativo o NO en caso negativo");
+        String conMayuscula = scanner.nextLine();
+        String mayusculas = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        boolean mayusculaEncontrada= false;
+        
+        //Con numeros
+        Scanner scannerNumero = new Scanner(System.in);
+        System.out.println("¿Deseas que tu contrasena tenga numeros? Coloca SI en caso afirmativo o NO en caso negativo");
+        String conNumeros = scannerNumero.nextLine();
+        String numeros= "1234567890";
+        boolean numeroEncontrado= false;
+        
+        //Con simbolos o caracteres especiales
+        Scanner scannerEspecial = new Scanner(System.in);
+        System.out.println("¿Deseas que tu contrasena tenga caracteres especiales? Coloca SI en caso afirmativo o NO en caso negativo");
+        String conEspeciales = scannerEspecial.nextLine();
+        String especiales = "!@#$%^&*";
+        boolean especialEncontrado= false;
+        
+        if( conMayuscula.equalsIgnoreCase("SI") ){
+            System.out.println("Tu contrasena tendra mayusculas");
+        }else{
+            System.out.println("Tu contrasena NO tendra mayusculas");
+        }
+        
+        
+        
     }
     
 }
